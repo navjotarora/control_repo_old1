6d0d282a065ebab12ee5e17e18f520e57abde089
navjotarora <46839000+navjotarora@users.noreply.github.com>
2020-05-09 16:55:42 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'this is a readme file for reference',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    ensure  => file,
-  }
+  
 }
